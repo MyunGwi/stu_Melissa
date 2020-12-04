758f9d1b05ee2a25bb4288c4f4b8f3736ef0ffa0
MyunGwi <mitsukopika@gmail.com>
2020-12-04 12:07:00 +0100

    test2

---
+++ Assets/Scripts/Scripts/Gameplay/HP_Bar_Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP_Bar_Player : MonoBehaviour
+{
+    public Transform hpBar;
+
+    void Start ()
+    {
+        if (hpBar == null) 
+        {
+            Debug.LogWarning("Il manque HP Bar !");
+            Debug.Break();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Item playerItem = GameObject.FindGameObjectWithTag("Player")?.GetComponent<Item>();
+        if (playerItem != null) 
+        {
+            hpBar.localScale = new Vector3( playerItem.HpRatio, 1f, 1f);
+        
+        }
+    }
+}
